7bf23c460be0936b10fa804ac4b4275586e90b9b
Advait Vedant <advait.suraj.vedant@gmail.com>
2024-04-04 17:06:31 -0700

    removed text that was broken

--- app/_components/info.tsx
+++ app/_components/info.tsx
@@ -12,7 +12,7 @@ export default function Info({ price, date }: InfoProps) {
     const month = tempDate.getMonth()
     const day = tempDate.getDate()
   return (
-    <div className="flex flex-col xl:flex-row h-fit p-8 md:p-12 xl:p-20 w-full bg-brown">
+    <div className="flex flex-col xl:flex-row h-fit p-8 md:p-12 xl:p-20 w-full ">
         <div className="w-fit h-fit p-2">
       <div className="flex flex-col h-fit w-full xl:w-96 p-10 backdrop-blur-md bg-pale/20 rounded-2xl shadow-lg">
         <Image
@@ -74,39 +74,4 @@ function GlassItem({ icon, title, info} : GlassItemProps){
            </div>
          </div>
     )
-}
-function Content() {
-  return (
-    <div className="min-w-[400px] w-full lg:w-1/2 text-brown flex text-wrap flex-col">
-      <h2 className="text-5xl pb-4 font-red font-bold">What is Harambe?</h2>
-      <h3 className="text-xl pb-4 font-red font-semibold">
-        Not your average primate, nor your favorite meme, or another crypto
-        wealth dream gone wrong. Harambe is a hedge fund system entirely driven
-        by artificial intelligence and designed to outsmart even the slickest
-        traders out there.
-      </h3>
-      <p className="text-md pb-2 font-red">
-        We've learned from the good, the bad, the winning, and the
-        not-so-winning cryptocurrency blueprints over time to develop a highly
-        profitable venture in the crypto jungle. Harambe has been trained on
-        years of historical trades and powerful strategies of !leading hedge
-        funds and expert traders. The ultimate goal? Making those high returns
-        typically reserved for elites accessible to all investors.
-      </p>
-      <p className="text-md pb-2 font-red">
-        Here's the beauty of it: all you have to do is buy it, hold it, and the
-        passive earnings are basically returning to you automatically. And nope,
-        it's not taking coffee breaks. It works non-stop, with zero effort on
-        your end. Deployed on the blockchain, Harambe operates around the clock,
-        making precise trades 24/7 on top cryptocurrencies like Bitcoin and
-        Ethereum.{" "}
-      </p>
-      <p className="text-md font-red">
-        The magic happens through a buy-back and burn mechanism, where profits
-        are funneled back to the community. It's the first decentralized
-        autonomous hedge fund, and the wealth flows directly to the token owners
-        without any detours.
-      </p>
-    </div>
-  );
 }
